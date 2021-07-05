f0b846c546863db3d0aedc3bbf061a75c5a7ebd3
roger.toledo <rogertoledo28@gmail.com>
2021-07-05 12:36:11 -0300

    projeto beer stock

---
+++ src/test/java/me/rto/beerstock/builder/BeerDTOBuilder.java
@@ -0,0 +1,36 @@
+package me.rto.beerstock.builder;
+
+import lombok.Builder;
+import me.rto.beerstock.dto.BeerDTO;
+import me.rto.beerstock.enums.BeerType;
+
+@Builder
+public class BeerDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String name = "Chope";
+
+    @Builder.Default
+    private String brand = "Kairós";
+
+    @Builder.Default
+    private int max = 50;
+
+    @Builder.Default
+    private int quantity = 10;
+
+    @Builder.Default
+    private BeerType type = BeerType.LAGER;
+
+    public BeerDTO toBeerDTO() {
+        return new BeerDTO(id,
+                name,
+                brand,
+                max,
+                quantity,
+                type);
+    }
+}
